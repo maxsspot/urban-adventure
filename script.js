b69e539955cd11d54a04f12b9c8a8323a234c0f5
nope <55818423+maxsspot@users.noreply.github.com>
2024-08-12 21:14:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // Opens Navigation
 function openNav () {
     document.getElementById ("navColumn").style.left = "0px";
+    document.getElementById ("overlay").style.opacity="0.5";
+    document.getElementById ("overlay").style.pointerEvents="all";
 }
 
 // Closes Navigation
 function closeNav () {
     document.getElementById ("navColumn").style.left = "-260px";
+    document.getElementById ("overlay").style.opacity="0.0";
+    document.getElementById ("overlay").style.pointerEvents="none";
 }
