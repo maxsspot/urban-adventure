a1b551c65b96c4a4f09ae89fa91c1f88e2852499
nope <55818423+maxsspot@users.noreply.github.com>
2024-08-12 21:06:14 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Opens Navigation
+function openNav () {
+    document.getElementById ("navColumn").style.left = "0px";
+}
+
+// Closes Navigation
+function closeNav () {
+    document.getElementById ("navColumn").style.left = "-260px";
+}
